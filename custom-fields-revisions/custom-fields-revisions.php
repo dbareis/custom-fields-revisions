8519c0542de7dec63e586937c0f2f071fad659f9
dbareis <dbareis@gmail.com>
2023-04-03 14:54:23 +1000

    Update custom-fields-revisions.php
    This now works on WP 6.2 and PHP 4.7

--- custom-fields-revisions/custom-fields-revisions.php
+++ custom-fields-revisions/custom-fields-revisions.php
@@ -1,53 +1,91 @@
 <?php
 /**
  * Plugin Name: Custom Fields Revisions
- * Plugin URI: http://github.com/vavrecan
- * Description: Show custom fields on the revision page
- * Version: 1.3
- * Author: Marek Vavrecan (vavrecan@gmail.com)
+ * Plugin URI: https://github.com/dbareis/custom-fields-revisions
+ * Description: Show (PODS etc) custom fields on the revision page.
+ * Version: 23.04.03
+ * Author: Dennis Bareis (dbareisNoSpam@ gmail dot com)
  **/
 
+
+//===========================================================================
+// NOTE: 
+// ~~~~~
+// You can use the following CSS if you don't want to see unchanged
+// custom fields listed:
+//       .diff-context { display: none; }
+//
+// This PHP file should be within the following directory for wordpress to
+// see it as a plugin:
+//       ".../wp-content/plugins/custom-fields-revisions"
+//===========================================================================
+
+
+//===========================================================================
+function cfr_filter_meta($meta)
 /**
  * Filter out all private custom fields that starts with underscore character
  * @param $meta
  * @return array
  */
-function cfr_filter_meta($meta) {
+//===========================================================================
+{
     $meta_filtered = [];
     foreach ($meta as $key => $value) {
-        if ($key{0} != "_")
+        if ($key[0] != "_")
             $meta_filtered[$key] = $value;
     }
     return $meta_filtered;
 }
 
+//===========================================================================
+function cfr_get_meta($post_id)
 /**
  * Get filtered custom fields from metadata
  * @param $post_id
  * @return array
  */
-function cfr_get_meta($post_id) {
+//===========================================================================
+{
     $meta = get_metadata("post", $post_id);
     $meta = cfr_filter_meta($meta);
     return $meta;
 }
 
+//===========================================================================
+function cfr_insert_meta($post_id, $meta)
 /**
  * Save custom fields with post / revision post
  * @param $post_id
  * @param $meta
  */
-function cfr_insert_meta($post_id, $meta) {
-    foreach ($meta as $meta_key => $meta_value) {
+//===========================================================================
+{
+    //MyLog("cfr_insert_meta() " . MyDump($meta, '$meta'));
+
+    foreach ($meta as $meta_key => $meta_value)
+    {
         if (is_array($meta_value))
+        {
             foreach ($meta_value as $single_meta_value)
-                add_metadata('post', $post_id, $meta_key, unserialize($single_meta_value));
+            {
+                //add_metadata('post', $post_id, $meta_key, unserialize($single_meta_value));
+                add_metadata('post', $post_id, $meta_key, $single_meta_value);
+                //MyLog("{$meta_key} []=> {$single_meta_value}");
+            }
+        }
         else
+        {
             add_metadata('post', $post_id, $meta_key, $meta_value);
+            //MyLog("{$meta_key} ==> {$meta_value}");
+        }
     }
 }
 
-function cfr_delete_meta($post_id) {
+//===========================================================================
+function cfr_delete_meta($post_id)
+//===========================================================================
+{
     $meta = cfr_get_meta($post_id);
 
     foreach ($meta as $meta_key => $meta_value) {
@@ -55,6 +93,8 @@ function cfr_delete_meta($post_id) {
     }
 }
 
+//===========================================================================
+function cfr_field($value, $field, $revision)
 /**
  * Creates text format from custom fields
  * @param $value
@@ -62,7 +102,7 @@ function cfr_delete_meta($post_id) {
  * @param $revision
  * @return string
  */
-function cfr_field($value, $field, $revision)
+//===========================================================================
 {
     $revision_id = $revision->ID;
     $meta = cfr_get_meta($revision_id);
@@ -76,18 +116,23 @@ function cfr_field($value, $field, $revision)
     return $return;
 }
 
+//===========================================================================
+function cfr_fields($fields)
 /**
  * Create new field in revision view with title Custom Fields
  * @param $fields
  * @return mixed
  */
-function cfr_fields($fields)
+//===========================================================================
 {
     $fields["custom_fields"] = "Custom Fields";
     return $fields;
 }
 
+
+//===========================================================================
 function cfr_restore_revision($post_id, $revision_id)
+//===========================================================================
 {
     $meta = cfr_get_meta($revision_id);
     cfr_delete_meta($post_id);
@@ -102,12 +147,14 @@ function cfr_restore_revision($post_id, $revision_id)
     }
 }
 
+//===========================================================================
+function cfr_save_post($post_id, $post)
 /**
  * Wordpress hook callback to save metadata to revision post
  * @param $post_id
  * @param $post
  */
-function cfr_save_post($post_id, $post)
+//===========================================================================
 {
     if ($parent_id = wp_is_post_revision($post_id)) {
         $meta = cfr_get_meta(get_post($parent_id)->ID);
@@ -118,6 +165,8 @@ function cfr_save_post($post_id, $post)
     }
 }
 
+//===========================================================================
+function cfr_post_has_changed($post_has_changed, $last_revision, $post)
 /**
  * Check if custom fields were changed to make sure new revision will be created
  * even when user did not modified title or content of the post
@@ -127,7 +176,8 @@ function cfr_save_post($post_id, $post)
  * @param $post
  * @return bool
  */
-function cfr_post_has_changed($post_has_changed, $last_revision, $post) {
+//===========================================================================
+{
     if (!$post_has_changed)
     {
         $meta = cfr_get_meta(get_post($last_revision)->ID);
@@ -142,10 +192,10 @@ function cfr_post_has_changed($post_has_changed, $last_revision, $post) {
     return $post_has_changed;
 }
 
-add_action('save_post', 'cfr_save_post', 10, 2);
-add_action('wp_restore_post_revision', 'cfr_restore_revision', 10, 2);
-add_filter('wp_save_post_revision_post_has_changed', 'cfr_post_has_changed', 10, 3);
 
-add_filter('_wp_post_revision_fields', 'cfr_fields', 10, 1);
-add_filter("_wp_post_revision_field_custom_fields", "cfr_field", 10, 3);
+add_action('save_post',                              'cfr_save_post',        10, 2);
+add_action('wp_restore_post_revision',               'cfr_restore_revision', 10, 2);
+add_filter('wp_save_post_revision_post_has_changed', 'cfr_post_has_changed', 10, 3);
+add_filter('_wp_post_revision_fields',               'cfr_fields',           10, 1);
+add_filter("_wp_post_revision_field_custom_fields",  'cfr_field',            10, 3);
 
